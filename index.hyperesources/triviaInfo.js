b57f743dcd6d34b5ae09eb7679963013635842cc
azschokke <54607309+azschokke@users.noreply.github.com>
2019-09-16 15:27:45 -0500

    Add files via upload

---
+++ index.hyperesources/triviaInfo.js
@@ -0,0 +1,88 @@
+function getInfo() {
+	window.hasClicked		 =  false; 
+	window.numberOfQuestions = 	10;
+	window.currentSlide = 		0;
+	window.currentScore = 		0;
+	window.title = 				"Mattel Toy Trivia";
+	
+	window.questions = [		"What year was the toy company Mattel created?", 
+								"How many companies has Mattel acquired? ", 
+								"How many toys did Mattel recall in 2007 for a link to lead contamination?", 
+								"Mattel became the master licensee for Harry Potter toys in what year?", 
+								"Which line of dolls is not owned by Mattel? " ,
+								"Barbie launched in what year?", 
+								"Which one of these careers has Barbie not had? ", 
+								"What year were Hot Wheels introduced?", 
+								"What year was the Matchbox brand introduced? ", 
+								"What year were Polly Pocket toys created?" ];
+								
+	window.correctAnswer = [ 	"B", "E", "D", "A", "C", "E", "D", "B", "B", "E" ]; 
+	
+	window.answerA = [ 			"1962", 
+								"21", 
+								"1 million", 
+								"2000", 
+								"Barbie" ,
+								"1964", 
+								"Rapper", 
+								"1954", 
+								"1945", 
+								"1983" ];
+								
+	window.answerB = [ 			"1945", 
+								"45", 
+								"40 million", 
+								"1998", 
+								"American Girl" ,
+								"1975", 
+								"Waitress", 
+								"1968", 
+								"1953", 
+								"1975" ];
+								
+	window.answerC = [ 			"1992", 
+								"0", 
+								"20 million", 
+								"2004", 
+								"Disney Princesses" ,
+								"1946", 
+								"Politician", 
+								"1975", 
+								"1968", 
+								"1964" ];
+								
+	window.answerD = [ 			"1936", 
+								"4", 
+								"18 million", 
+								"2002", 
+								"Polly Pocket" ,
+								"1982", 
+								"Orchestra member", 
+								"1985", 
+								"1972", 
+								"1992" ];
+								
+	window.answerE = [ 			"2002", 
+								"23", 
+								"15 million", 
+								"2006", 
+								"Ever After High" ,
+								"1959", 
+								"Bee Keeper", 
+								"1992", 
+								"1980", 
+								"1989" ];
+								
+	window.response = [ 		"Mattel was founded by Harold Matson and Elliot Handler in January 1945. The company originally sold picture frames. ", 
+								"Mattel’s first acquisition was Dee & Cee Toy Co. Ltd. In 1962.", 
+								"A transition to manufacturing in China lead to the lead contamination scandal.", 
+								"Warner Bros extended their partnership to include Batman, Superman, Justice League, and Looney Tunes in 2002.", 
+								"Mattel lost its Disney brand license to Hasbro at the end of 2015, after the dolls sold underwhelmingly. " ,
+								"Barbie was inspired by a German doll called Bild Lilli.", 
+								"Barbie has held over 200 positions in 9 different areas of employment.", 
+								"Matchbox was the biggest competitor to Hot Wheels until they were acquired by Mattel in 1997.", 
+								"Matchbox got their name because the boxes the die cast toys came in resembled matchboxes. ", 
+								"Polly Pockets were discontinued in the United States in 2015, but made a comeback in 2018. " ];
+							
+}
+						
